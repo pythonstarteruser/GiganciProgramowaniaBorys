c12b61372e2c55b768abdac7b8ce231c87ef8262
pythonstarteruser <borys.stanczak@uczen.eduwarszawa.pl>
2026-09-06 13:37:06 +0200

    nowy commit

--- Lekcja 5/Program.cs
+++ Lekcja 5/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello github!");
+        Console.WriteLine("Podaj Swoje Imię:");
+        string imie = Console.ReadLine();
+        Console.WriteLine("$ Cześć {imie}!");
+        Console.WriteLine("Jak się dziś czujesz w skali od 0 do 100");
+        int samopoczucie = int.Parse(Console.ReadLine());
     }
 }
