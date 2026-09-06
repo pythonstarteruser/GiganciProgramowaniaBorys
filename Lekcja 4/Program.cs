51c2476e9a73dc253517980a4767ede4419aebdb
pythonstarteruser <borys.stanczak@uczen.eduwarszawa.pl>
2026-09-06 12:03:25 +0200

    Pierwszy commit

---
+++ Lekcja 4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Data;
+
+namespace Lekcja_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Podaj ile masz lat:");
+        // int wiek = int.Parse(Console.ReadLine());
+        // Console.Clear();
+        // Console.WriteLine($"Jeżeli masz {wiek} lat to:");
+        // int tygodnie = wiek * 52;
+        // Console.WriteLine($"Czyli Masz {tygodnie} tygodni");
+        // int dni = wiek * 365;
+        // Console.WriteLine($"Czyli masz {dni} dni");
+        // int godziny = dni * 24;
+        // Console.WriteLine($"Czyli masz {godziny} godzin");
+        // int minuty = godziny * 60;
+        // Console.WriteLine($"Czyli masz {minuty} minut");
+        // int sekundy = minuty * 60;
+        // Console.WriteLine($"Czyli masz {sekundy} sekund");
+        // long milisekundy = sekundy * 1000L;
+        // Console.WriteLine($"Czyli masz {milisekundy} milisekund");
+        // long nanosekundy = milisekundy * 1000000L;
+        // Console.WriteLine($"Czyli masz {nanosekundy} nanosekund");
+        // Console.WriteLine("ala" == "kot");
+        // Console.WriteLine(1 == 1);
+        // Console.WriteLine(1 != 1);
+        // Console.WriteLine(1 > 1);
+        // Console.WriteLine(1 < 1);
+        // Console.WriteLine(1 >= 1);
+        // Console.WriteLine(1 <= 1);     
+        // bool wynik = 5 > 3;
+        // Console.WriteLine(wynik);
+        // Console.WriteLine("Podaj ile masz lat");
+        // int wiek = int.Parse(Console.ReadLine());
+        // bool Pelnoletnosc = wiek >= 18;
+        // Console.WriteLine($"Czy jesteś pełnoletni ->{Pelnoletnosc}");
+        // Console.WriteLine("Podaj ile masz lat");
+        // int wiek = int.Parse(Console.ReadLine());
+        // Console.WriteLine("Podaj ile masz pieniędzy");
+        // double PLN = double.Parse(Console.ReadLine());
+        // bool mozeMinonki = wiek >= 7 && PLN >=15;
+        // Console.WriteLine(mozeMinonki);
+        // bool mozeHarry = wiek >= 13 && PLN >=21;
+        // Console.WriteLine(mozeHarry);
+        // bool mozeAvatar = wiek >= 16 && PLN >=20;
+        // Console.WriteLine(mozeAvatar);
+        // bool Pelnoletnosc = true;
+        // string wynik = Pelnoletnosc ? "tak" : "nie";
+        // Console.WriteLine($"Czy jesteś pełnoletni? -> {wynik}");
+        // Console.WriteLine("Podaj login:");
+        // string login = Console.ReadLine();
+
+        // Console.WriteLine("Podaj hasło:");
+        // string haslo = Console.ReadLine();
+
+        // bool poprawneDane = login == "KawaWjechała" && haslo == "TętnoJestWNormie";
+
+        // string wynik = poprawneDane ? "Zalogowano pomyślnie" : "Niepoprawny login lub hasło";
+
+        // Console.WriteLine(wynik);
+        // Console.WriteLine("Podaj długość a");
+        // double a = double.Parse(Console.ReadLine());
+        // Console.WriteLine("Podaj długość b");
+        // double b = double.Parse(Console.ReadLine());
+        // Console.WriteLine("Podaj długość c");
+        // double c = double.Parse(Console.ReadLine());
+        // bool czyProstokatny = Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2);
+        // Console.WriteLine($"Czy trójkąt jest Prostokątny? -> {czyProstokatny}");
+    }
+}
---
+++ Lekcja 5/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Lekcja_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
