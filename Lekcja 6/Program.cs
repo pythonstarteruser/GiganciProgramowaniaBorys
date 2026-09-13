4d8518b31d8b6c01167af7a61bf9864f3008f2b0
pythonstarteruser <borys.stanczak@uczen.eduwarszawa.pl>
2026-09-13 14:10:12 +0200

    gbgh

--- Lekcja 6/Program.cs
+++ Lekcja 6/Program.cs
@@ -4,19 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Podaj liczbę: ");
-        int liczba = int.Parse(Console.ReadLine());
-        if (liczba > 5)
+        // Console.Write("Podaj liczbę: ");
+        // int liczba = int.Parse(Console.ReadLine());
+        // if (liczba > 5)
+        // {
+        //     Console.WriteLine("Liczba jest większa niż 5");
+        // }
+        // else if (liczba == 5)
+        // {
+        //     Console.WriteLine("Liczba jest równa 5");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Liczba jest mniejsza niż 5");
+        string haslo = ("ABRAKADABRA");
+        Console.WriteLine("Podaj Hasło");
+        string wpiszhaslo = Console.ReadLine();
+        if (haslo == wpiszhaslo)
         {
-            Console.WriteLine("Liczba jest większa niż 5");
-        }
-        else if (liczba == 5)
-        {
-            Console.WriteLine("Liczba jest równa 5");
+            Console.WriteLine("Zostałeś Poprawnie zalogowany");
+        
         }
         else
         {
-            Console.WriteLine("Liczba jest mniejsza");
+            Console.WriteLine("Hasło jest Niepoprawne. Spróbuj Ponownie.");
+        }
+
         }
     }
-}
+
