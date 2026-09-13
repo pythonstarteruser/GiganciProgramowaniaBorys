1155f6a883d1a8a90617a7f4b6c383e938a7a78b
pythonstarteruser <borys.stanczak@uczen.eduwarszawa.pl>
2026-09-13 13:39:32 +0200

    ghg;

---
+++ Lekcja 6/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lekcja_6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int liczba = 10;
+        if (liczba > 5)
+        {
+            Console.WriteLine("Liczba jest większa niż 5");
+        }
+        else
+        {
+            Console.WriteLine("Liczba jest mniejsza lub równa 5");
+        }
+    }
+}
--- Lekcja5.1/Program.cs
+++ Lekcja5.1/Program.cs
@@ -4,25 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Console.WriteLine("Podaj ocenę z matematyki:");
-        // double matematyka = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj ocenę z matematyki:");
+        double matematyka = double.Parse(Console.ReadLine());
 
-        // Console.WriteLine("Podaj ocenę z fizyki:");
-        // double fizyka = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj ocenę z fizyki:");
+        double fizyka = double.Parse(Console.ReadLine());
 
-        // Console.WriteLine("Podaj ocenę z chemii:");
-        // double chemia = double.Parse(Console.ReadLine());
-        // Console.WriteLine("Podaj swoją frekwencję w procentach:");
-        // double frekwencja = double.Parse(Console.ReadLine());
-        // double srednia = (matematyka + fizyka + chemia) / 3;
+        Console.WriteLine("Podaj ocenę z chemii:");
+        double chemia = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj swoją frekwencję w procentach:");
+        double frekwencja = double.Parse(Console.ReadLine());
+        double srednia = (matematyka + fizyka + chemia) / 3;
 
-        // Console.WriteLine($"Twoja średnia końcowa wynosi: {srednia:F2}");
+        Console.WriteLine($"Twoja średnia końcowa wynosi: {srednia:F2}");
 
-        // bool stypendium = srednia >= 4.5 && frekwencja >= 80;
-        // string komunikat = stypendium
-        //     ? "Gratulacje! Twoja średnia jest wystarczająca do uzyskania stypendium."
-        //     : "Niestety, Twoja średnia jest za niska do uzyskania stypendium.";
+        bool stypendium = srednia >= 4.5 && frekwencja >= 80;
+        string komunikat = stypendium
+            ? "Gratulacje! Twoja średnia jest wystarczająca do uzyskania stypendium."
+            : "Niestety, Twoja średnia jest za niska do uzyskania stypendium.";
 
-        // Console.WriteLine(komunikat);
+        Console.WriteLine(komunikat);
+        Console.ReadKey();
     }
 }
